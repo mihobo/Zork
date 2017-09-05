08ec6b7e000decbdd9ba2dc50dce59db8fbc4753
Miho Tse <mihotse@gmail.com>
2017-09-05 12:41:18 +0100

    User can win or lose a game, add partials, add routes and actions

--- app/controllers/quests_controller.rb
+++ app/controllers/quests_controller.rb
@@ -2,4 +2,12 @@ class QuestsController < ApplicationController
   def show
     @quests = Quest.find(params[:id])
   end
+
+  def gameover
+    @quests = Quest.find(3)
+  end
+
+  def success
+    @quests = Quest.find(2)
+  end
 end
---
+++ spec/features/game_over_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.feature "game over", :type => :feature do
+
+  before do
+    quest1 = Quest.create(id: 1, quest_message: "In some field", path_A: "Go in", path_A_ID: 2, path_B: 'Retreat', path_B_ID: 3 )
+    quest3 = Quest.create(id: 3, quest_message: "Game Over", path_A: "New Game", path_A_ID: 1, path_B: nil, path_B_ID: nil )
+  end
+  
+  scenario 'user loses the game' do
+    visit '/'
+    click_button 'Start Game'
+    click_button 'Retreat'
+    expect(page).to have_content('You coward! You have lost respect from all the village elders! You are forever cursed!!')
+  end
+end
