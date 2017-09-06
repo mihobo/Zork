ae885cb0aaa9b4d489a69f3a1c5af84d7d1f68f3
Miho Tse <mihotse@gmail.com>
2017-09-06 10:32:10 +0100

    Start implementation of Bot chatty response

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,15 @@
 module WelcomeHelper
+  def get_quest_message
+    @quest_message = Quest.pluck(:quest_message).map(&:to_s).join
+  end
+
+  def get_bot_talk
+    response = ''
+    get_quest_message.each_char{ |x| response << x ; sleep 0.05 }
+    response
+  end
+
+  def return_bot_talk
+    params[:display] = get_bot_talk
+  end
 end
--- spec/features/success_spec.rb
+++ spec/features/success_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
-RSpec.feature "game over", :type => :feature do
+RSpec.feature "success", :type => :feature do
 
   before do
     quest1 = Quest.create(id: 1, quest_message: "In some field", path_A: "Go in", path_A_ID: 2, path_B: 'Retreat', path_B_ID: 3 )
     quest2 = Quest.create(id: 2, quest_message: "Hoorah", path_A: "New Game", path_A_ID: 1, path_B: nil, path_B_ID: nil )
   end
-  
+
   scenario 'user wins the game' do
     visit '/'
     click_button 'Start Game'
